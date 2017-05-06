220edec89d8309b2cd808a51e377f6c8556b4956
jaytea123 <jaytea123@users.noreply.github.com>
2017-05-05 19:28:07 -0700

    Working Program
    Got a working Etch-a-Sketch with random rgb values

--- script.js
+++ script.js
@@ -5,27 +5,52 @@ $(document).ready(function() {
 			container.append('<div class="grid"></div>').children();
 		}	
 	}
+	//Get container height and width
+	var containerWidth = parseInt(container.css("width"));
+	var containerHeight = parseInt(container.css("height"));
+
+	var gridWidth = containerWidth / 16;
+	var gridHeight = containerHeight / 16;
 	
 	var grid = $('.grid');
+	grid.css('width', gridWidth);
+	grid.css('height', gridHeight);
+	
 	//change grid color
+	
 	grid.mouseenter(function() {
-		$(this).css("backgroundColor", "green");
+		var gridColorOne = Math.floor((Math.random() * 255) + 1);
+		var gridColorTwo = Math.floor((Math.random() * 255) + 1);
+		var gridColorThree = Math.floor((Math.random() * 255) + 1);
+		$(this).css("backgroundColor", "rgb(" + gridColorOne + ", " + gridColorTwo + ", " + gridColorThree);
 	});
 		
 	$(reset).click(function() {
 		var input = prompt("Please enter grid size", "16");
 		var gridSize = parseInt(input);
 		container.empty();
+
+
+		var gridWidth = containerWidth / gridSize;
+		var gridHeight = containerHeight / gridSize;
+
 		
 		for(var i = 0; i < gridSize; i++) {
 			for(var j = 0; j < gridSize; j++) {
 				container.append('<div class="grid"></div>').children();
 			}	
 		}
+
 		var grid = $('.grid');
+
+		grid.css('width', gridWidth);
+		grid.css('height', gridHeight);
 		//change grid color
 		grid.mouseenter(function() {
-			$(this).css("backgroundColor", "green");
+			var gridColorOne = Math.floor((Math.random() * 255) + 1);
+			var gridColorTwo = Math.floor((Math.random() * 255) + 1);
+			var gridColorThree = Math.floor((Math.random() * 255) + 1);
+			$(this).css("backgroundColor", "rgb(" + gridColorOne + ", " + gridColorTwo + ", " + gridColorThree);
 		});
 	});
 });
